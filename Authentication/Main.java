045b23df851710eb54da96fb987d5b35e9c895d6
Davtyan360 <76738628+Davtyan360@users.noreply.github.com>
2021-05-23 13:37:44 +0400

    Update Main.java

--- Authentication/Main.java
+++ Authentication/Main.java
@@ -1,4 +1,4 @@
-package Registration;
+package Authentication;
 
 import java.util.Scanner;
 
@@ -6,11 +6,11 @@ public class Main implements PasswordCrypt {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Login or Reg : ");
-        String z = sc.nextLine();
+        String z = sc.next();
         while(z!="exit"){
             if(z=="login")new Login();
             if(z=="reg")new Registeration();
-
+            z = sc.next();
         }
     }
 }
