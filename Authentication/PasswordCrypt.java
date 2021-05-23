a648abe6aee267c5a817417d534604d2dd697cf3
Davtyan360 <76738628+Davtyan360@users.noreply.github.com>
2021-05-23 13:40:56 +0400

    Update PasswordCrypt.java

--- Authentication/PasswordCrypt.java
+++ Authentication/PasswordCrypt.java
@@ -1,6 +1,5 @@
-package Registration;
+package Authentication;
 
-import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
