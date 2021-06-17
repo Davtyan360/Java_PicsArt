ede178c7d74cbcd7f50d2b5800f100f300883f22
Davtyan360 <76738628+Davtyan360@users.noreply.github.com>
2021-06-17 12:10:04 +0400

    Create Timer.java

---
+++ Authentication/Timer.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        Thread s = new Thread(() -> st(5));
+        s.start();
+    }
+    public static void st(int minute){
+        minute*=60;
+        for (int i = minute; i > 0; i--){
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            StringBuilder s = new StringBuilder();
+            s.append(i/60);
+            s.append(':');
+            if(i%60<10)
+                s.append('0');
+            s.append(i%60);
+            System.out.println(s.toString());
+        }
+    }
+}
