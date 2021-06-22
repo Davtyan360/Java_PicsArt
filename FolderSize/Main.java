d436363231aef8befb25766517cbe37b0ac5c907
Davtyan360 <76738628+Davtyan360@users.noreply.github.com>
2021-06-22 18:33:03 +0400

    Delete FolderSize directory

--- FolderSize/Main.java
+++
@@ -1,8 +0,0 @@
-package FolderSize;
-
-public class Main {
-    public static void main(String[] args) {
-        Thread th = new Thread(() -> new WriteFileSize().writeSize("C:\\yura"));
-        th.run();
-    }
-}
